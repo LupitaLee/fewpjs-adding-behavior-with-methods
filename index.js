304de882210135f5e8afca88a2054849cbcc4408
Lupita <lupitarivera8899@gmail.com>
2021-07-13 10:26:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+
+
+class Cat {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+    //...
+  }
+
+    // let one = new Cat('saha')
+    // one.speak()
+  
+  class Dog {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird extends Cat{
+    
+    speak(){
+        if( this.sex === "male"){
+        return `It's me! ${this.name}, the parrot!`  } 
+         else if (this.sex === "female"){
+        return `${this.name} says squawk!`}
+    } 
+  } 
